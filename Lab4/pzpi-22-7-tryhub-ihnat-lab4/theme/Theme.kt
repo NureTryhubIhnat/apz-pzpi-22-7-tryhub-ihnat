6b2152c38c820ff5a63c03d5a225ecb64658ce8e
NureTryhubIhnat <ihnat.tryhub@nure.ua>
2025-06-03 22:49:20 +0200

    Update Theme.kt

--- Lab4/pzpi-22-7-tryhub-ihnat-lab4/theme/Theme.kt
+++ Lab4/pzpi-22-7-tryhub-ihnat-lab4/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.autocare.autocare1.ui.theme
+package com.healthcare.healthcare1.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun AutoCare1Theme(
+fun healthCare1Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
